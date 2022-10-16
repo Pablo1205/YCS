6db86d483e2c2fb9629fdcbef377caf448938fde
Robebou <guillaume.theret@efrei.net>
2022-10-16 14:49:48 +0200

    clean up + start auth/passport
    Co-Authored-By: PabloEfrei <74155592+PabloEfrei@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ const app = express();
 const session = require('express-session')
 const path = require('path')
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const crypto = require('crypto-js');
 var MySQLStore = require('express-mysql-session')(session);
 const bodyParser = require("body-parser");
 const logger = require('morgan')
@@ -27,7 +25,7 @@ app.use(session({
 }))
 
 
-// const authRoutes = require("./routes/authentification.js");
+const authRoutes = require("./routes/authentification.js");
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
@@ -40,27 +38,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(express.static(path.join(__dirname, '../client')))
 
-// app.use('/auth', authRoutes)
+app.use('/auth', authRoutes)
 
 app.listen(3030, function () {
     console.log("Server is running on port " + 3030);
 });
-
-/*
-var route, routes = [];
-app._router.stack.forEach(function(middleware){
-    if(middleware.route){ // routes registered directly on the app
-        routes.push(middleware.route);
-    } else if(middleware.name === 'router'){ // router middleware 
-        middleware.handle.stack.forEach(function(handler){
-            route = handler.route;
-            route && routes.push(route);
-        });
-    }
-});
-console.log(route, routes)
-
-*/
-
 
 module.exports = app
