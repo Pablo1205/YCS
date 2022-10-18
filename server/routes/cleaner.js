6a390eb0eebc596fab8043fdc636eb5a925816de
PabloEfrei <pablo.sanchez@efrei.net>
2022-10-18 09:02:51 +0100

    available cleaner query

--- server/routes/cleaner.js
+++ server/routes/cleaner.js
@@ -3,8 +3,19 @@ const router = express.Router()
 var connection = require('../db');
 const passport = require('passport');
 
-
-router.get('/available', (req, res) => {
+// gérer entité cleaner et afficher disponibilité
+// resultats de query dans result
+router.get('/available/:idCleaner', (req, res) => {
+    connection.query(`SELECT cleaners.idCleaner, cleanersSchedule.idSchedule, cleanersSchedule.day,cleanersSchedule.StartTime, cleanersSchedule.EndTime FROM cleaners JOIN cleanersSchedule ON cleaners.idCleaner=cleanersSchedule.idCleaner WHERE cleaners.idCleaner=` + req.params.idCleaner , async (error, results) => {
+        if (error) throw error;
+        if (results.length == 0) {
+            return res.status(409).json({ message: 'ID inexistant' })
+        }else {
+            res.send(results)
+            console.log(results)//this 
+            return results
+        }
+    })
 })
 
 module.exports = router
