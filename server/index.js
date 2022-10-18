579ec93715690c870517f735b8428aa0312b944b
Robebou <guillaume.theret@efrei.net>
2022-10-18 08:20:26 +0200

    cleanerRoutes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const logger = require('morgan')
 const cors = require('cors');
 
 app.use(session({
-    key:'session_cookie',
+    key: 'session_cookie',
     store: new MySQLStore({
         host: process.env.HOST_SQL,
         port: 3306,
@@ -20,18 +20,19 @@ app.use(session({
         password: process.env.PASSWORD,
     }),
     secret: 'grehjznejzkhgjrez',
-    saveUninitialized: false,   
+    saveUninitialized: false,
     resave: false,
     secure: false
 }))
 
 
 const authRoutes = require("./routes/authentification.js");
+const cleanerRoutes = require("./routes/cleaner.js");
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -40,6 +41,7 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(express.static(path.join(__dirname, '../client')))
 
 app.use('/auth', authRoutes)
+app.use("/cleaner", cleanerRoutes)
 
 app.listen(3030, function () {
     console.log("Server is running on port " + 3030);
---
+++ server/routes/cleaner.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+var connection = require('../db');
+const passport = require('passport');
+
+
+router.get('/available', (req, res) => {
+})
+
+module.exports = router
