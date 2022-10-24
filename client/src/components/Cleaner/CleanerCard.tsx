02147519303c76c431832d1d3075473e5603a42d
Robebou <guillaume.theret@efrei.net>
2022-10-24 18:38:13 +0200

    finish card

--- client/src/components/Cleaner/CleanerCard.tsx
+++ client/src/components/Cleaner/CleanerCard.tsx
@@ -5,18 +5,22 @@ export default function CleanerCard() {
     const prenom = "Guillaume"
     const bio = "Salut test bio"
     const ville = "Paris"
-    const rayon = "10 km"   
+    const rayon = "10 km"
     const joinDate = new Date();
     const profilPicture = "https://cdn-icons-png.flaticon.com/512/3135/3135715.png";
     return (
-        <div style={{width: "100%"}}>
-            <div>
-                <p>{prenom} {nom}</p>
-                <img src={profilPicture} style={{width: "30%", borderRadius: 1000, border:"1px solid black"}} />
+        <div style={{ width: "100%", border: "3px solid black", marginBottom: 10, marginTop: 10, borderRadius: "10px", overflow: "hidden", display: "flex", flexDirection: "row" }}>
+            <div style={{ backgroundColor: "#107ACA", width: "40%", padding: "10px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                <p style={{ color: "white", fontWeight: "bold" }}>{prenom.toUpperCase()} {nom.toUpperCase()}</p>
+                <div style={{ width: "40%", padding: "4px", backgroundColor: "#0D4DFD", borderRadius: 1000 }}>
+                    <img src={profilPicture} style={{ width: "100%", borderRadius: 1000, border: "1px solid black" }} />
+                </div>
             </div>
-            <div>
-
+            <div style={{ backgroundColor: "white", width: "60%", display: "flex", flexDirection: "column", justifyContent: "space-around", alignItems: "center" }}>
+                <p style={{ fontWeight: "bold" }}>Bio</p>
+                <p>{bio}</p>
+                <p>Joined : {joinDate.toLocaleDateString("fr-FR")}</p>
             </div>
-        </div>
+        </div >
     )
 }
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -12,18 +12,18 @@ export default function Home({ isAuth }: { isAuth: boolean }) {
     }
   }, [isAuth]);
   return (
-    <div>
-      <div style={{ backgroundColor: "#107ACA", paddingLeft: "20%" }}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <div style={{ backgroundColor: "#107ACA", width:"100%", paddingLeft:"20%" }}>
         <p style={{ color: "white", fontWeight: "bold", fontSize: 45 }}>Find an appointment with our cleaning staff</p>
-        <div>
+        <div style={{backgroundColor: "white",display: "inline-block", border: "4px solid", padding:"5px"}}>
           <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
           <button>Search</button>
         </div>
       </div>
-      <div style={{display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"flex-start"}}>
+      <div style={{display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"space-between", alignItems: "center", width: "90%"}}>
         {arrayCleaner.length !== 0 && arrayCleaner.map(cleaner => {
           return (
-            <div key={cleaner} style={{ maxWidth: "200", minWidth: "300px" }}>
+            <div key={cleaner} style={{minWidth: "45%" }}>
               <CleanerCard />
             </div>
           )
